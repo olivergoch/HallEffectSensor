7414f735d43b3d1fb6374e0d6d085ea75d26857f
olivergoch <the.oliver.goch@gmail.com>
2018-09-06 14:37:44 -0700

    Changed formatting

--- HallEffectSensor.ino
+++ HallEffectSensor.ino
@@ -1,11 +1,13 @@
 int hallPin = 14;    //A0 is 14
 double hallValue = 0;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
   hallValue = analogRead(hallPin);
   Serial.println(hallValue);
   delay(2000);
